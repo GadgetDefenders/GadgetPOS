3aadacc7b6eec60f3b3670aef929024b01bbcf89
GadgetDefenders <gadgetdefendersllc@gmail.com>
2026-08-04 19:55:57 -0500

    Load configurable repair intake workflow

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import ReportsWidget from './ReportsWidget';
+import RepairIntakeWidget from './RepairIntakeWidget';
 import './styles.css';
 import './pos.css';
 import './posTabs.css';
+import './repairIntake.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
     <ReportsWidget />
+    <RepairIntakeWidget />
   </React.StrictMode>,
 );
