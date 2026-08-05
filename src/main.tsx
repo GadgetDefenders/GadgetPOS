d8bb9fd80a002f97a6282ec5569350b0465368a2
GadgetDefenders <gadgetdefendersllc@gmail.com>
2026-08-04 22:08:44 -0500

    Connect inventory manager

--- src/main.tsx
+++ src/main.tsx
@@ -3,16 +3,19 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import RepairIntakeWidget from './RepairIntakeWidget';
 import ReportsNavWidget from './ReportsNavWidget';
+import InventoryManagerWidget from './InventoryManagerWidget';
 import './styles.css';
 import './pos.css';
 import './posTabs.css';
 import './repairIntake.css';
 import './commercial.css';
+import './inventoryManager.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
     <RepairIntakeWidget />
     <ReportsNavWidget />
+    <InventoryManagerWidget />
   </React.StrictMode>,
 );
