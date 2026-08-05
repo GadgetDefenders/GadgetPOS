6e3099da357a980fc4e5b1586c3c985683357985
GadgetDefenders <gadgetdefendersllc@gmail.com>
2026-08-04 21:58:36 -0500

    Load polished commercial layout and main reports page

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import ReportsWidget from './ReportsWidget';
 import RepairIntakeWidget from './RepairIntakeWidget';
 import './styles.css';
 import './pos.css';
 import './posTabs.css';
 import './repairIntake.css';
+import './commercial.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-    <ReportsWidget />
     <RepairIntakeWidget />
   </React.StrictMode>,
 );
