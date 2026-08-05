f9825a7f6aa7b06cb2486495ffd49bd817c54c57
GadgetDefenders <gadgetdefendersllc@gmail.com>
2026-08-04 19:43:37 -0500

    Load tabbed POS categories

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import App from './App';
 import ReportsWidget from './ReportsWidget';
 import './styles.css';
 import './pos.css';
+import './posTabs.css';
+import './posTabs';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
